416de30ce130d5a0a4d7fd72690543efea9d277d
davicro <david.rollonla@gmail.com>
2026-08-26 14:57:13 +0200

    Simple cli program (only works with r34 for now)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{println};
+
+use booru_rs::{Client, GelbooruClient, Post, prelude::*};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name="booru-cli")]
+#[command(version="0.1-alpha")]
+#[command(about="Command line tool to interact with various boorus.")]
+struct Cli {
+
+    tags: Vec<String>,
+    #[arg(long)]
+    user_id: String,
+    #[arg(long)]
+    api_key: String
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let booru = Rule34Client::builder()
+    .tags(cli.tags)?
+    .limit(1)
+    .set_credentials(cli.api_key, cli.user_id)
+    .build();
+
+    let posts = booru.get().await?;
+    for post in &posts {
+        println!("{}", post.file_url);
+    }
+
+    Ok(())
 }
