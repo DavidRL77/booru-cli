73e60c551c59733a2bd66ca1c9443c1225c83e22
davicro <david.rollonla@gmail.com>
2026-09-15 17:39:51 +0200

    fix: spelling mistake in invalid argument error

--- src/error.rs
+++ src/error.rs
@@ -9,7 +9,7 @@ pub enum CliError {
 impl fmt::Display for CliError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CliError::InvalidArgument(s) => write!(f, "Invalid arguemnt: '{}'", s)
+            CliError::InvalidArgument(s) => write!(f, "Invalid argument: '{}'", s)
         }
     }
 }
