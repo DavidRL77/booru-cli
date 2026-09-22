57a8890d966f12cef439cc69c002ce5361c01bb5
davicro <david.rollonla@gmail.com>
2026-09-22 19:57:25 +0200

    fix: non-unix function that depended on unix-only dependency

--- src/uzers.rs
+++ src/uzers.rs
@@ -1,11 +1,11 @@
 /// Get the current user's unix id
 #[cfg(unix)]
-pub(crate) fn get_uid() -> uzers::uid_t {
+pub(crate) fn get_uid() -> u32 {
     uzers::get_current_uid()
 }
 
 /// Non-unix platforms will always return 0
 #[cfg(not(unix))]
-pub(crate) fn get_uid() -> uzers::uid_t {
+pub(crate) fn get_uid() -> u32 {
     0
 }
